5f25c81692d98139db707e722e3d8388497e3062
ArezooNazer <arezoo.nazerdeylami@gmail.com>
2018-10-21 15:27:43 +0330

    handling different stream type

--- app/src/main/java/com/example/user/exovideoplayer/demo/MainActivity.java
+++ app/src/main/java/com/example/user/exovideoplayer/demo/MainActivity.java
@@ -1,15 +1,71 @@
 package com.example.user.exovideoplayer.demo;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.user.exovideoplayer.R;
+import com.example.user.exovideoplayer.player.VideoPlayer;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.util.Util;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PlayerView playerView;
+    private VideoPlayer player;
+    private String uri = "https://static.cdn.asset.aparat.com/avt/12202725_15s.mp4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playerView = findViewById(R.id.demo_player_view);
+        player = new VideoPlayer(playerView, getApplicationContext(),uri);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            player.initializePlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            player.initializePlayer();
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+           player.pausePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            player.releasePlayer();
+        }
+    }
+
 }
--- app/src/main/java/com/example/user/exovideoplayer/player/VideoPlayer.java
+++ app/src/main/java/com/example/user/exovideoplayer/player/VideoPlayer.java
@@ -1,4 +1,115 @@
 package com.example.user.exovideoplayer.player;
 
+import android.content.Context;
+import android.net.Uri;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.example.user.exovideoplayer.R;
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.offline.FilteringManifestParser;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaPeriod;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.MediaSourceEventListener;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.source.dash.manifest.DashManifestParser;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.source.hls.playlist.DefaultHlsPlaylistParserFactory;
+import com.google.android.exoplayer2.source.smoothstreaming.DefaultSsChunkSource;
+import com.google.android.exoplayer2.source.smoothstreaming.SsMediaSource;
+import com.google.android.exoplayer2.source.smoothstreaming.manifest.SsManifestParser;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.Allocator;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.upstream.TransferListener;
+import com.google.android.exoplayer2.util.Util;
+
+import java.io.IOException;
+
 public class VideoPlayer {
+
+    private String CLASS_NAME = VideoPlayer.class.getName();
+
+    private PlayerView playerView;
+    private SimpleExoPlayer player;
+    private Context context;
+    private DataSource.Factory dataSourceFactory;
+    private DefaultBandwidthMeter bandwidthMeter;
+    private MediaSource mediaSource;
+    private DefaultTrackSelector trackSelector;
+    private DefaultTrackSelector.Parameters trackSelectorParameters;
+    private boolean playWhenReady;
+    private int currentWindow;
+    private long playbackPosition;
+    private Uri uri;
+
+    public VideoPlayer(PlayerView playerView, Context context, String uri) {
+        this.playerView = playerView;
+        this.context = context;
+        this.uri = Uri.parse(uri);
+    }
+
+    public void initializePlayer() {
+        if (player == null)
+            player = ExoPlayerFactory.newSimpleInstance(context, new DefaultTrackSelector());
+
+        dataSourceFactory = new DefaultDataSourceFactory(context,
+                Util.getUserAgent(context, CLASS_NAME), new DefaultBandwidthMeter());
+
+        mediaSource = buildMediaSource(uri, null);
+
+        playerView.setPlayer(player);
+        player.setPlayWhenReady(true);
+        playerView.setKeepScreenOn(true);
+        player.prepare(mediaSource);
+
+    }
+
+    private MediaSource buildMediaSource(Uri uri, @Nullable String overrideExtension) {
+        @C.ContentType int type = Util.inferContentType(uri, overrideExtension);
+        switch (type) {
+            case C.TYPE_SS:
+                return new SsMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
+            case C.TYPE_DASH:
+                return new DashMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
+            case C.TYPE_HLS:
+                return new HlsMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
+            case C.TYPE_OTHER:
+                return new ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
+            default: {
+                throw new IllegalStateException("Unsupported type: " + type);
+            }
+        }
+    }
+
+    public void pausePlayer() {
+        player.setPlayWhenReady(false);
+    }
+
+    public void releasePlayer() {
+        if (player != null) {
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            playWhenReady = player.getPlayWhenReady();
+            playerView.setPlayer(null);
+            player.release();
+            player = null;
+
+        }
+    }
+
+
 }
