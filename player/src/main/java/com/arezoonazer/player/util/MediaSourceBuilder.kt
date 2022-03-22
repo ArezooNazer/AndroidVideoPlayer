d50bbc8b5d8e5f3da72e49ec8ce3be5a3bbe9cfd
Arezoo Nazer <arezoo.nazer@cafebazaar.ir>
2022-03-22 10:41:20 +0430

    Add MediaSourceBuilder

---
+++ player/src/main/java/com/arezoonazer/player/util/MediaSourceBuilder.kt
@@ -0,0 +1,39 @@
+package com.arezoonazer.player.util
+
+import android.net.Uri
+import androidx.core.net.toUri
+import com.arezoonazer.player.argument.PlayerParams
+import com.arezoonazer.player.extension.toSubtitleMediaItem
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.util.MimeTypes
+import com.google.android.exoplayer2.util.Util
+
+suspend fun createMediaItem(playerParams: PlayerParams): MediaItem {
+    val uri = playerParams.url.toUri()
+    return MediaItem.Builder()
+        .setUri(uri)
+        .setMimeType(getUriMimeType(uri))
+        .setSubtitleConfigurations(playerParams.subtitles.toSubtitleMediaItem())
+        .build()
+}
+
+private fun getUriMimeType(uri: Uri): String {
+    return when (val type = Util.inferContentType(uri)) {
+        C.TYPE_HLS -> {
+            MimeTypes.APPLICATION_M3U8
+        }
+        C.TYPE_DASH -> {
+            MimeTypes.APPLICATION_MPD
+        }
+        C.TYPE_SS -> {
+            MimeTypes.APPLICATION_SS
+        }
+        C.TYPE_OTHER -> {
+            MimeTypes.APPLICATION_MP4
+        }
+        else -> {
+            throw IllegalStateException("Unsupported type: $type")
+        }
+    }
+}
