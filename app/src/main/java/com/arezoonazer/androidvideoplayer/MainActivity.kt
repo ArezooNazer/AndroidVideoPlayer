47aa61623e09c4a7fd6c4babae1e1d81ecaf0d10
Arezoo Nazer <arezoo.nazer@cafebazaar.ir>
2022-03-24 14:16:08 +0430

    Add capability to insert video and subtitle urls

--- app/src/main/java/com/arezoonazer/androidvideoplayer/MainActivity.kt
+++ app/src/main/java/com/arezoonazer/androidvideoplayer/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.arezoonazer.androidvideoplayer
 
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
+import com.arezoonazer.androidvideoplayer.databinding.ActivityMainBinding
 import com.arezoonazer.player.argument.PlayerParams
 import com.arezoonazer.player.argument.VideoSubtitle
 import com.arezoonazer.player.extension.startPlayer
@@ -9,10 +13,24 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        startPlayer(getPlayerParam())
+        setContentView(binding.root)
+
+        with(binding) {
+            playButton.setOnClickListener { startPlayer(getPlayerParam()) }
+
+            videoUrlEditText.doOnTextChanged { text, _, _, _ ->
+                customPlayButton.isEnabled = text.isNullOrEmpty().not()
+            }
+
+            customPlayButton.setOnClickListener { onCustomPlayButtonClicked() }
+        }
     }
 
     private fun getPlayerParam(): PlayerParams {
@@ -29,5 +47,45 @@ class MainActivity : AppCompatActivity() {
             url = "https://5b44cf20b0388.streamlock.net:8443/vod/smil:bbb.smil/playlist.m3u8",
             subtitles = subtitleList
         )
+    }
+
+    private fun onCustomPlayButtonClicked() {
+        with(binding) {
+            val videoUrl = videoUrlEditText.text.toString()
+
+            if (videoUrl.isEmpty()) {
+                showToast("Please insert video url")
+                return
+            }
+
+            val subtitleUrl1 = subtitle1UrlEditText.text.toString()
+            val subtitleUrl2 = subtitle2UrlEditText.text.toString()
+
+            val subtitleList = mutableListOf<VideoSubtitle>().apply {
+                if (subtitleUrl1.isEmpty().not()) {
+                    add(
+                        VideoSubtitle(
+                            title = "subtitle 1",
+                            url = subtitleUrl1
+                        )
+                    )
+                }
+
+                if (subtitleUrl2.isEmpty().not()) {
+                    add(
+                        VideoSubtitle(
+                            title = "subtitle 2",
+                            url = subtitleUrl2
+                        )
+                    )
+                }
+            }
+
+            startPlayer(PlayerParams(videoUrl, subtitleList))
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, LENGTH_SHORT).show()
     }
 }
